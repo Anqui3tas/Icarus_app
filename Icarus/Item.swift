09f583e97d03b356482bc27ea94f4e3bfdaad48d
Quentin Brooks <quentin@lanteacorp.com>
2025-02-24 20:56:45 -0800

    Initial Commit

---
+++ Icarus/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Icarus
+//
+//  Created by Quentin Brooks on 2/24/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ IcarusTests/IcarusTests.swift
@@ -0,0 +1,16 @@
+//
+//  IcarusTests.swift
+//  IcarusTests
+//
+//  Created by Quentin Brooks on 2/24/25.
+//
+
+import Testing
+
+struct IcarusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
